95a716d6e405e02e259b4f31cd3fc0483a764b6d
DrK-ToN <everton.drk@gmail.com>
2025-01-05 19:30:55 -0300

    Criação das páginas de alunos e remanejamento das rotas no index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const rota_turma = require("./routes/rota_turma");
+const rota_aluno = require("./routes/rota_aluno");
 
 const app = express();
 
@@ -15,9 +16,12 @@ app.set("views", path.join(__dirname, "views"));
 //Remanejando Rotas de cargo
 app.use("/rota_turma", rota_turma);
 
+//Remanejando Rotas do aluno
+app.use("/rota_aluno", rota_aluno);
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(
-        "Servidor Rodando na em http://localhost:3000/rota_turma/turma"
+        "Servidor Rodando na em http://localhost:3000/rota_turma/turma\nhttp://localhost:3000/rota_aluno/aluno"
     );
 });
